91f85e90121e5000a42410280d6708120fd4fd38
SuperCatMaster <supercatmaster@mail.ru>
2019-04-18 20:00:43 +0800

    Separeted css, html and js

---
+++ scripts.js
@@ -0,0 +1,44 @@
+function changeColor(){
+  var r = document.getElementById("r").value;
+  var g = document.getElementById("g").value;
+  var b = document.getElementById("b").value;
+  var br = r - "10";
+  var bg = g - "10";
+  var bb = b - "10";
+  document.getElementById("egg").style.backgroundColor = "rgb("+ r +", "+ g +", "+ b +")";
+  document.getElementById("footer").style.backgroundColor = "rgb("+ r +", "+ g +", "+ b +")";
+  document.body.style.backgroundColor = "rgb("+ br +", "+ bg +", "+ bb +")";
+  setTimeout("changeColor()", 50);
+}
+function setRed(){
+  document.getElementById("r").value = 255;
+  document.getElementById("g").value = 0;
+  document.getElementById("b").value = 0;
+}
+function setGreen(){
+  document.getElementById("r").value = 0;
+  document.getElementById("g").value = 255;
+  document.getElementById("b").value = 0;
+}
+function setBlue(){
+  document.getElementById("r").value = 0;
+  document.getElementById("g").value = 0;
+  document.getElementById("b").value = 255;
+}
+function setWhite(){
+  document.getElementById("r").value = 255;
+  document.getElementById("g").value = 255;
+  document.getElementById("b").value = 255;
+}
+function setBlack(){
+  document.getElementById("r").value = 0;
+  document.getElementById("g").value = 0;
+  document.getElementById("b").value = 0;
+}
+function getRGB(){
+  var r = document.getElementById("r").value;
+  var g = document.getElementById("g").value;
+  var b = document.getElementById("b").value;
+  var rgb = r + g + b;
+  alert("Значение цветовой схемы RGB: " + "#" + rgb);
+}
